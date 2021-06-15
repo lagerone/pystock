f4b07cd446632cb1a0eb2af41ee55b2873b8c89e
Daniel Lager <daniel.lager@tretton37.com>
2021-06-15 09:53:35 +0200

    fixes stuff

--- src/main.py
+++ src/main.py
@@ -73,7 +73,7 @@ def _create_graph_prices(ticker: str):
 
 
 def _is_stock_live(stored_prices: List[int], live_stock_price: float):
-    live_graph_price = round(int(live_stock_price) * 100)
+    live_graph_price = int(live_stock_price * 100)
     return stored_prices[-3:] != [
         live_graph_price,
         live_graph_price,
@@ -82,61 +82,86 @@ def _is_stock_live(stored_prices: List[int], live_stock_price: float):
 
 
 # pylint: disable=too-many-locals
-def _render_live_data_graph(ticker: str, prices: List[float]):
+def _render_live_data_graph(ticker: str, closing_prices: List[float], show_graph: bool):
     live_data_file_path = os.path.join(
         f"{TICKER_DATA_PATH}", f"ticker_{ticker}_live_{_get_file_cache_key()}"
     )
     live_stock_data = fetch_live_data(ticker=ticker)
     live_stock_price = live_stock_data.get_live_stock_price()
 
-    closing_stock_price: float = (
-        live_stock_data.regular_market_price.market_price
-        if live_stock_data.market_type != MarketType.REGULAR
-        else live_stock_data.regular_market_previous_close
-    )
-    graph_prices = _create_graph_prices(ticker)
-
-    live_fig = tpl.figure()
-    live_fig.plot(range(len(graph_prices) + 1), graph_prices, width=50)
-
     def ensure_two_dec(num: float) -> str:
         return "{:.2f}".format(num)
-
-    previous_price_delta = prices[-1] - prices[-2]
 
     stored_prices = read_live_data_file(file_path=live_data_file_path)
     is_stock_live = _is_stock_live(
         stored_prices=stored_prices, live_stock_price=live_stock_price.market_price
     )
 
-    change_color = TColor.GREEN if live_stock_price.market_change > 0 else TColor.RED
     live_color = TColor.GREEN if is_stock_live else ""
     live_color_end = TColor.END if is_stock_live else ""
-    close_change_color = TColor.GREEN if previous_price_delta >= 0 else TColor.RED
+
+    market_type_map = {
+        MarketType.PREPRE.value: "PREPRE".rjust(7, " "),
+        MarketType.PRE.value: "PRE".rjust(7, " "),
+        MarketType.REGULAR.value: "REGULAR",
+        MarketType.POST.value: "POST".rjust(7, " "),
+        MarketType.CLOSED.value: "CLOSED".rjust(7, " "),
+    }
+
+    closing_stock_price: float = (
+        live_stock_data.regular_market_price.market_price
+        if live_stock_data.market_type != MarketType.REGULAR
+        else live_stock_data.regular_market_previous_close
+    )
+
+    close_delta = closing_stock_price - live_stock_data.regular_market_previous_close
+    # TODO: Fix this.
+    p_index = (
+        -3 if live_stock_data.market_type in [MarketType.PRE, MarketType.PREPRE] else -2
+    )
+    prev_close_delta = (
+        live_stock_data.regular_market_previous_close - closing_prices[p_index]
+    )
+
+    def render_delta(delta: float):
+        color = TColor.GREEN if delta >= 0 else TColor.RED
+        return f"{color}{ensure_two_dec(delta)}{TColor.END}"
+
+    def render_good_or_bad(text: str, is_good: bool):
+        color = TColor.GREEN if is_good else TColor.RED
+        return f"{color}{text}{TColor.END}"
+
+    percent_output = f"({ensure_two_dec(live_stock_price.market_change_percent)}%)"
+
     print(
-        f"  {live_color}{TColor.BOLD}{ticker}{TColor.END}{live_color_end}"
-        f"   {live_stock_data.market_type}"
+        f"  {live_color}{TColor.BOLD}{ticker.ljust(3, ' ')}{TColor.END}{live_color_end}"
+        f"   {market_type_map[live_stock_data.market_type]}"
         f"     {TColor.BOLD}{ensure_two_dec(live_stock_price.market_price)}{TColor.END}"
-        f"     {change_color}{ensure_two_dec(live_stock_price.market_change)}"
-        f"     ({ensure_two_dec(live_stock_price.market_change_percent)}%){TColor.END}"
+        f"     {render_delta(live_stock_price.market_change)}"
+        f"     {render_good_or_bad(percent_output, live_stock_price.market_change_percent >= 0)}"
     )
     print(
-        f"  CLOSED      "
+        f"  CLOSED          "
         f"  {ensure_two_dec(closing_stock_price)}"
-        f"     {close_change_color}{ensure_two_dec(previous_price_delta)}{TColor.END}"
+        f"     {render_delta(close_delta)}"
     )
     print(
-        f"  PREVCLOSE   "
-        f"  {ensure_two_dec(live_stock_data.regular_market_previous_close)}"
+        f"  PREVCLOSE       "
+        f"  {ensure_two_dec(live_stock_data.regular_market_previous_close)}     {render_delta(prev_close_delta)}"
     )
     print("")
-    live_fig.show()
+
+    if show_graph:
+        graph_prices = _create_graph_prices(ticker)
+        live_fig = tpl.figure()
+        live_fig.plot(range(len(graph_prices) + 1), graph_prices, width=50)
+        live_fig.show()
 
 
 # pylint: enable=too-many-locals
 
 
-def _main(ticker: str) -> None:
+def _render_ticker(ticker: str, show_graph: bool = True) -> None:
     ticker_file_path = _create_ticker_data_file(
         ticker=ticker, cache_key=_get_file_cache_key()
     )
@@ -144,20 +169,30 @@ def _main(ticker: str) -> None:
 
     day_counter = -20
     days: List[int] = []
-    prices: List[float] = []
+    closing_prices: List[float] = []
     for line in lines[1:]:
         days.append(day_counter)
         day_counter += 1
-        prices.append(float(line.split(",")[4]))
+        closing_prices.append(float(line.split(",")[4]))
 
-    close_price_fig = tpl.figure()
-    close_price_fig.plot(days, prices, width=50)
+    _render_live_data_graph(
+        ticker=ticker, closing_prices=closing_prices, show_graph=show_graph
+    )
 
-    _render_live_data_graph(ticker=ticker, prices=prices)
-    print("\n")
-    print("      Closing prices, last 20 days.")
-    close_price_fig.show()
+    if show_graph:
+        close_price_fig = tpl.figure()
+        close_price_fig.plot(days, closing_prices, width=50)
+        print("\n")
+        print("      Closing prices, last 20 days.")
+        close_price_fig.show()
+
+
+def _main(tickers=List[str]):
+    for ticker in tickers:
+        logging.debug(f"Rendering ticker {ticker}...")
+        _render_ticker(ticker=ticker, show_graph=len(tickers) == 1)
+        print("\n")
 
 
 if __name__ == "__main__":
-    _main(ticker=sys.argv[1] if len(sys.argv) > 1 else os.environ["DEFAULT_TICKER"])
+    _main(tickers=sys.argv[1:] if len(sys.argv) > 1 else [os.environ["DEFAULT_TICKER"]])
